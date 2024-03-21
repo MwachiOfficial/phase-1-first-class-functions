7037a3cff8a150bfa075fd0ca649d136c57fd579
matthewmwachi <mattmuhande@gmail.com>
2024-03-22 00:15:37 +0300

    first class

---
+++ index.js
@@ -0,0 +1,16 @@
+function receivesAFunction(callback){
+    return (callback())
+}
+receivesAFunction(function(){})
+
+function returnsANamedFunction(named){
+     return function name(){
+
+     }
+}
+
+function returnsAnAnonymousFunction(callBack){
+    return function() {
+
+    }
+}
